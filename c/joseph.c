0bd35d93dda69daa4e9f2b8f8a74f8eef701c9a3
c0d3runn3r <joseph.sullivan@active911.com>
2015-04-30 08:47:27 -0700

    Better benchmarks, comments

--- c/joseph.c
+++ c/joseph.c
@@ -1,5 +1,4 @@
-// Goal: Create a file containing 10,000 random 4-letter strings, sorted alphabetically.
-// 
+/* Goal: Create a file containing 10,000 random 4-letter strings, sorted alphabetically. */ 
 
 #include <stdio.h>
 #include <time.h>
@@ -10,39 +9,34 @@
 #define ARRAY_LENGTH 10000
 #define STRING_LENGTH 4
 
-// Handle GNU quirks
+/* Handle GNU quirks */
 #ifndef __GNUC__
 #define __compar_fn_t int(*)
 #endif
 
-// Comparison function
-int cmpfunc (const void *a, const void *b);
+/* Time measurements */
+double bench();
 
 
 int main () {
 
-	// Benchmark #TODO may have a resolution of 10ms on some systems
-	double start_time, end_time;
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	start_time=tv.tv_sec+(tv.tv_usec/1000000.0);
+	bench();
 
-
-	// Allocate space for our strings
-	char* a=(void*)malloc(ARRAY_LENGTH * (STRING_LENGTH+1));		// Don't forget trailing NUL or LF
+	/* Allocate space for our strings */
+	char* a=(void*)malloc(ARRAY_LENGTH * (STRING_LENGTH+1));		/* Don't forget trailing NUL or LF */
 	if(a==NULL) {
 
 		printf("Unable to allocate memory\n");
 		exit(1);
 	} 
 
-	// Define an alphabet
+	/* Define an alphabet */
 	char alphabet[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	// Initialize our (weak) random generator #TODO and create many strings
+	/* Initialize our (weak) random generator #TODO and create many strings */
 	srand(time(NULL));
 
-	char* line=a;	// Set to beginning of array
+	char* line=a;	/* Set to beginning of array */
 	int n=0;
 	int y=0;
 	while(n< ARRAY_LENGTH * (STRING_LENGTH+1)){
@@ -52,48 +46,79 @@ int main () {
 			n++;
 		}
 
-		// Add terminating LF (change this to 0x0 if you want to print...)
+		/* Add terminating LF (change this to 0x0 if you want to print...) */
 		a[n]=0x0a;
 		n++;		
 	}
 
-	// Sort
+	double gen=bench();
+
+	/* Sort */
 	qsort(a, ARRAY_LENGTH, STRING_LENGTH+1, (__compar_fn_t)strcmp);
 
-	// Print (change LF to NULL above first!)
-	// n=0;
-	// while(n<ARRAY_LENGTH * (STRING_LENGTH+1)){
+	double sort=bench();
 
-	// 	printf("String: %s\n",&a[n]);
-	// 	n+=(STRING_LENGTH+1);
-	// }
+	/*
+	Print (change LF to NULL above first!)
+	n=0;
+	while(n<ARRAY_LENGTH * (STRING_LENGTH+1)){
 
-	// Write to file
+		printf("String: %s\n",&a[n]);
+		n+=(STRING_LENGTH+1);
+	}
+	*/
+
+	/* Write to file */
 	FILE *f;
 	f=fopen("strings.txt", "w");
 	fwrite(a, 1, (ARRAY_LENGTH * (STRING_LENGTH+1)), f);
 	fclose(f);
 
+	double write=bench();
 
-	// Clean up
+
+	/* Clean up */
 	free(a);
 
-	// Finish benchmark
-	gettimeofday(&tv, NULL);
-	end_time=tv.tv_sec+(tv.tv_usec/1000000.0);
-	printf("Complete in %.1f ms\n", ((end_time - start_time) * 1000 )); 
+	double complete=bench();
+
+
+	/* Finish benchmark */
+	printf("Generate: %.1f ms\n", gen); 
+	printf("Sort: %.1f ms\n", sort); 
+	printf("Write file: %.1f ms\n", write); 
+	printf("Total: %.1f ms\n", gen+sort+write+complete); 
 
 	return 0;
 }
 
 
+/** 
+ * Benchmark
+ *
+ * Returns the number of milliseconds that have elapsed since the last call
+ * #TODO may have a resolution of 10ms on some systems 
+ */
+double bench() {
 
-int cmpfunc (const void *a, const void *b) {
+	// Store the last bench time
+	static double last_time;
 
+	// Get time
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
 
+	// Get Start time, if first run
+	if(last_time==0) {
 
+		last_time=tv.tv_sec+(tv.tv_usec/1000000.0);
+		return 0;
+	}
 
-
+	double this_time=tv.tv_sec+(tv.tv_usec/1000000.0);
+	double elapsed_ms=(this_time-last_time)*1000;
+	last_time=this_time;
+	return elapsed_ms;
 }
 
 
